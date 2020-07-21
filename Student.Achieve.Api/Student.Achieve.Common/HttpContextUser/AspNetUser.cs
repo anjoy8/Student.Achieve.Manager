137465e6533b9ccad34725e7d4c0c448cec94741
anjoy8 <3143422472@qq.com>
2020-07-21 16:04:41 +0800

    user

--- Student.Achieve.Api/Student.Achieve.Common/HttpContextUser/AspNetUser.cs
+++ Student.Achieve.Api/Student.Achieve.Common/HttpContextUser/AspNetUser.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
@@ -14,11 +15,54 @@ namespace Student.Achieve.Common.HttpContextUser
             _accessor = accessor;
         }
 
-        public string Name => _accessor.HttpContext.User.Identity.Name;
+        public string Name => GetName();
+
+        private string GetName()
+        {
+            if (IsAuthenticated())
+            {
+                return _accessor.HttpContext.User.Identity.Name;
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(GetToken()))
+                {
+                    return GetUserInfoFromToken("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").FirstOrDefault().ObjToString();
+                }
+            }
+
+            return "";
+        }
+
+        public int ID => GetClaimValueByType("jti").FirstOrDefault().ObjToInt();
 
         public bool IsAuthenticated()
         {
             return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+
+        public string GetToken()
+        {
+            return _accessor.HttpContext.Request.Headers["Authorization"].ObjToString().Replace("Bearer ", "");
+        }
+
+        public List<string> GetUserInfoFromToken(string ClaimType)
+        {
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            if (!string.IsNullOrEmpty(GetToken()))
+            {
+                JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(GetToken());
+
+                return (from item in jwtToken.Claims
+                        where item.Type == ClaimType
+                        select item.Value).ToList();
+            }
+            else
+            {
+                return new List<string>() { };
+            }
         }
 
         public IEnumerable<Claim> GetClaimsIdentity()
